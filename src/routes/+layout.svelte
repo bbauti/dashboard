c3f67296f485e5d7e9a837c7febb1cf259a4907a
Bautista Igarzabal <bautistaigarzabal11@gmail.com>
2023-07-11 13:40:11 -0300

    testing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,17 @@
 <script>
 	import '../app.scss';
+
+	import { page } from "$app/stores"
+
+	if (!$page.route.id.includes("auth")) {
+		if (!$page.data.session) {
+			console.log("no estas logueado")
+		} else {
+			console.log("tas logueado")
+		}
+	} else {
+		console.log("estas en "+$page.route.id)
+	}
 </script>
 
 <slot />
