20fa3eb0f353e989c3e623ecc595264bbff7d943
Bautista Igarzabal <bautistaigarzabal11@gmail.com>
2023-07-11 15:59:09 -0300

    test redirect

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -4,25 +4,33 @@ import { GITHUB_ID, GITHUB_SECRET, SUPABASE_SERVICE_ROLE_KEY, SUPABASE_URL, SUPA
 import { redirect } from "@sveltejs/kit";
 import { sequence } from "@sveltejs/kit/hooks";
 import { SupabaseAdapter } from "@auth/supabase-adapter"
+import { goto } from '$app/navigation';
 
 import jwt from "jsonwebtoken"
 
+function goBack(defaultRoute = '/') {
+  const ref = document.referrer;
+  goto(ref.length > 0 ? ref : defaultRoute)
+}
+
 async function authorization({ event, resolve }) {
-  // Protect any routes under /authenticated
+  // si no esta logueado te obliga
+  const session = await event.locals.getSession();
+
   if (!event.url.pathname.startsWith("/auth")) {
-    const session = await event.locals.getSession();
     if (!session) {
       throw redirect(303, "/auth");
     }
+  } else {
+    if (session) {
+      goBack()
+    }
   }
 
-  // If the request is still here, just proceed as normally
   return resolve(event);
 }
 
-// First handle authentication, then authorization
-// Each function acts as a middleware, receiving the request handle
-// And returning a handle which gets passed to the next function
+
 export const handle = sequence(
   SvelteKitAuth({
     providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
