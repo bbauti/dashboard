fb1299d8de6137ac0d1c134edea5e8bb42b429fe
Bautista Igarzabal <bautistaigarzabal11@gmail.com>
2023-07-11 14:01:40 -0300

    test force login

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -9,7 +9,7 @@ import jwt from "jsonwebtoken"
 
 async function authorization({ event, resolve }) {
   // Protect any routes under /authenticated
-  if (event.url.pathname.startsWith("/authenticated")) {
+  if (!event.url.pathname.startsWith("/auth")) {
     const session = await event.locals.getSession();
     if (!session) {
       throw redirect(303, "/auth");
--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,1 +1,9 @@
+<script>
+    import { signIn, signOut } from "@auth/sveltekit/client"
+    import { page } from "$app/stores"
+</script>
 <p>hola</p>
+<p>
+    <span class="notSignedInText">You are not signed in</span>
+    <button on:click={() => signIn("github")}>Sign In with GitHub</button>
+</p>
