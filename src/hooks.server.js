57260989d2290af769ef2d3cc28af2a1ce982da7
Bautista Igarzabal <bautistaigarzabal11@gmail.com>
2023-07-11 10:44:49 -0300

    supabase

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,8 +1,11 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
 import GitHub from "@auth/core/providers/github";
-import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private";
+import { GITHUB_ID, GITHUB_SECRET,  } from "$env/static/private";
 import { redirect } from "@sveltejs/kit";
 import { sequence } from "@sveltejs/kit/hooks";
+import { SupabaseAdapter } from "@auth/supabase-adapter"
+
+import jwt from "jsonwebtoken"
 
 async function authorization({ event, resolve }) {
   // Protect any routes under /authenticated
@@ -23,6 +26,26 @@ async function authorization({ event, resolve }) {
 export const handle = sequence(
   SvelteKitAuth({
     providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
+    adapter: SupabaseAdapter({
+        url: process.env.NEXT_PUBLIC_SUPABASE_URL,
+        secret: process.env.SUPABASE_SERVICE_ROLE_KEY,
+      }),
+      callbacks: {
+        async session({ session, user }) {
+          const signingSecret = process.env.SUPABASE_JWT_SECRET
+          if (signingSecret) {
+            const payload = {
+              aud: "authenticated",
+              exp: Math.floor(new Date(session.expires).getTime() / 1000),
+              sub: user.id,
+              email: user.email,
+              role: "authenticated",
+            }
+            session.supabaseAccessToken = jwt.sign(payload, signingSecret)
+          }
+          return session
+        },
+      },
   }),
   authorization
 );
