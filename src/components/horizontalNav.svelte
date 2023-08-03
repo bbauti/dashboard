b182fa5be97dfb54aad49d4ad7955ab73892b09a
bbauti <bautistaigarzabal11@gmail.com>
2023-08-03 13:37:59 -0300

    navbar colors

--- src/components/horizontalNav.svelte
+++ src/components/horizontalNav.svelte
@@ -7,14 +7,51 @@
 	$: path = $page.url.pathname;
 
 	const nav = [
-		{ title: 'Inicio', url: '/app', icon: 'majesticons:home', class: 'mt-[1.5rem]' },
+		{
+			title: 'Inicio',
+			url: '/app',
+			icon: 'majesticons:home',
+			class: 'mt-[1.5rem]',
+			color: 'text-blue-300',
+			active: 'text-blue-500'
+		},
 		{ divider: true },
-		{ title: 'Carrito', url: '/app/sell', icon: 'fluent:cart-16-filled' },
-		{ title: 'Stock', url: '/app/stockmgm', icon: 'solar:box-bold' },
-		{ title: 'Team', url: '/app/team', icon: 'fluent:people-team-24-filled' },
-		{ title: 'Ventas', url: '/app/sales', icon: 'solar:card-bold' },
+		{
+			title: 'Carrito',
+			url: '/app/sell',
+			icon: 'fluent:cart-16-filled',
+			color: 'text-green-300',
+			active: 'text-green-500'
+		},
+		{
+			title: 'Stock',
+			url: '/app/stockmgm',
+			icon: 'solar:box-bold',
+			color: 'text-purple-300',
+			active: 'text-purple-500'
+		},
+		{
+			title: 'Team',
+			url: '/app/team',
+			icon: 'fluent:people-team-24-filled',
+			color: 'text-orange-300',
+			active: 'text-orange-500'
+		},
+		{
+			title: 'Ventas',
+			url: '/app/sales',
+			icon: 'solar:card-bold',
+			color: 'text-pink-300',
+			active: 'text-pink-500'
+		},
 		{ divider: true },
-		{ title: 'Ajustes', url: '/app/settings', icon: 'ic:round-settings' }
+		{
+			title: 'Ajustes',
+			url: '/app/settings',
+			icon: 'ic:round-settings',
+			color: 'text-gray-300',
+			active: 'text-gray-500'
+		}
 	];
 </script>
 
@@ -24,10 +61,13 @@
 			<a
 				href={link.url}
 				class="{link.class ? link.class : ''} {path === link.url
-					? 'active'
+					? 'active shadow-xl shadow-primary/[0.02]'
 					: ''} hover:scale-[1.03] transform-gpu"
 			>
-				<iconify-icon icon={link.icon} />
+				<iconify-icon
+					class={path === link.url ? `${link.active} transition-all ease-in-out` : link.color}
+					icon={link.icon}
+				/>
 				{link.title}
 			</a>
 		</li>
